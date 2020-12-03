edddd28e98221060b86bd44d779d77e61f53dd8b
Alison Williams <apw5011@gmail.com>
2020-12-03 00:19:04 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,27 @@
 
+class CashRegister
+  attr_accessor :total, :discount, :title, :price, :quantity 
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end 
+  
+  def add_item(title, price, quantity = 0)
+    @title = title
+    @price = price
+    @quantity = quantity
+    
+    if quantity > 0
+      self.total += price * quantity 
+    else
+      self.total += price 
+    end 
+  end 
+  
+  def apply_discount
+    @total -= @total * @discount / 100 
+    
+  end 
+  
+end 
